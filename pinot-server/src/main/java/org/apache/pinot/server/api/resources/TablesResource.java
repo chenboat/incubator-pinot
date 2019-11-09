7f25e660fa6d9de73c4b6387c584ea821677e78d
TING CHEN <tingchen@uber.com>
2019-11-08 18:09:00 -0800

    Add a rest api on server to download segments.

--- pinot-server/src/main/java/org/apache/pinot/server/api/resources/TablesResource.java
+++ pinot-server/src/main/java/org/apache/pinot/server/api/resources/TablesResource.java
@@ -23,6 +23,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -31,21 +34,34 @@ import java.util.Map;
 import java.util.Set;
 import javax.inject.Inject;
 import javax.ws.rs.DefaultValue;
+import javax.ws.rs.Encoded;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import org.apache.commons.io.FileUtils;
+import org.apache.pinot.common.config.TableConfig;
+import org.apache.pinot.common.metadata.ZKMetadataProvider;
 import org.apache.pinot.common.restlet.resources.ResourceUtils;
 import org.apache.pinot.common.restlet.resources.TableSegments;
 import org.apache.pinot.common.restlet.resources.TablesList;
+import org.apache.pinot.common.utils.CommonConstants;
+import org.apache.pinot.common.utils.StringUtil;
+import org.apache.pinot.common.utils.TarGzCompressionUtils;
+import org.apache.pinot.common.utils.URIUtils;
 import org.apache.pinot.core.data.manager.InstanceDataManager;
 import org.apache.pinot.core.data.manager.SegmentDataManager;
 import org.apache.pinot.core.data.manager.TableDataManager;
 import org.apache.pinot.core.segment.index.SegmentMetadataImpl;
+import org.apache.pinot.filesystem.PinotFS;
+import org.apache.pinot.filesystem.PinotFSFactory;
 import org.apache.pinot.server.starter.ServerInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -175,4 +191,30 @@ public class TablesResource {
       }
     }
   }
+
+  @GET
+  @Produces(MediaType.APPLICATION_OCTET_STREAM)
+  @Path("/segments/{tableName}/{segmentName}")
+  @ApiOperation(value = "Download a segment", notes = "Download a segment")
+  public Response downloadSegment(
+      @ApiParam(value = "Name of the table", required = true) @PathParam("tableName") String tableName,
+      @ApiParam(value = "Name of the segment", required = true) @PathParam("segmentName") @Encoded String segmentName,
+      @Context HttpHeaders httpHeaders) throws Exception {
+    TableDataManager tableDataManager = checkGetTableDataManager(tableName);
+    String tableDir = tableDataManager.getTableDataDir();
+    String tarFilePath = TarGzCompressionUtils.createTarGzOfDirectory(tableDir + "/" + segmentName);
+    File tarFile = new File(tarFilePath);
+    Response.ResponseBuilder builder = Response.ok();
+    builder.entity((StreamingOutput) output -> {
+      try {
+        Files.copy(tarFile.toPath(), output);
+      } finally {
+        FileUtils.deleteQuietly(tarFile);
+      }
+    });
+    builder.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + tarFile.getName());
+    builder.header(HttpHeaders.CONTENT_LENGTH, tarFile.length());
+    return builder.build();
+  }
+
 }
